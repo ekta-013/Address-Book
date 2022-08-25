36522a6174315bf1beeff04a66c3b46a33218c6d
ekta <ektamo0123@gmail.com>
2022-08-25 06:33:40 +0530

    Added : Ability to add new contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,19 +1,37 @@
 package com.bridgelaps;
 
+import java.util.Scanner;
 
 public class AddressBook {
 	public void addDetails() {
 
 		ContactDetails person = new ContactDetails();
 
-        person.setFirstName("Ekta");
-        person.setLastName("Mohite");
-        person.setAddress("Takka Colony,Panvel");
-        person.setCity("Panvel");
-        person.setState("Maharashtra");
-        person.setZip("410206");
-        person.setPhoneNumber("7887690540");
-        person.setEmail("ektamo0123@gmail.com");
+		Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Name : ");
+        person.setFirstName(sc.nextLine());
+
+        System.out.println("Enter Last Name : ");
+        person.setLastName(sc.nextLine());
+
+        System.out.println("Enter Address : ");
+        person.setAddress(sc.nextLine());
+
+        System.out.println("Enter City Name : ");
+        person.setCity(sc.nextLine());
+
+        System.out.println("Enter State Name : ");
+        person.setState(sc.nextLine());
+
+        System.out.println("Enter Zip Code : ");
+        person.setZip(sc.nextLine());
+
+        System.out.println("Enter Phone Number : ");
+        person.setPhoneNumber(sc.nextLine());
+
+        System.out.println("Enter Email Id : ");
+        person.setEmail(sc.nextLine());
 
         System.out.println(person);
     }
